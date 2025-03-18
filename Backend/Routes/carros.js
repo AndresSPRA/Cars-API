e60e2b46c46337033f395dc329c7a126701f1694
AndresSPRA <Andres.serpa259@pascualbravo.edu.co>
2025-03-17 20:12:04 -0500

    refactor: Se creó nueva carpeta routes con los endpoints de car
    Para codigo limpio se esta haciendo un cambio en el index.js

---
+++ Backend/Routes/carros.js
@@ -0,0 +1,60 @@
+import { Router } from "express"
+import { Car } from "../modelos/carsModelo.js";
+import { Brand } from "../modelos/brandModelo.js";
+
+const carRouter = Router()
+
+app.get("/api/autos", async (req, res) => {
+  try {
+    // Buscar todos los autos y poblar la información de la marca
+    const cars = await Car.find().populate("idBrand", "nombreBrand");
+
+    if (cars.length === 0) {
+      return res.status(404).send({ message: "No hay autos registrados" });
+    }
+
+    return res.status(200).send({ cars });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ message: error.message });
+  }
+})
+
+carRouter.post("/crear", async (req, res) => {
+  try {
+    // Extraer los datos de la solicitud
+    let { modelo, descripcion, precio, kilometraje, marca } = req.body
+    let idBrand;
+
+    // Si hay datos faltantes, rechazar la solicitud
+    if (!modelo || !descripcion || !precio || !kilometraje || !marca) {
+      return res.status(400).send("Ingresar todos los datos")
+    }
+
+    // Obtener el identificador de la marca
+    const marcaEncontrada = await Brand.findOne({ nombreBrand: "toyota" })
+    if (!marcaEncontrada) {
+      return res.status(404).send({ message: "Marca no encontrada" })
+    }
+
+    idBrand = marcaEncontrada._id;
+
+    // Crear nuevo carro
+    const car = await Car.create({
+      modelo,
+      descripcion,
+      precio,
+      kilometraje,
+      idBrand,
+    });
+    return res.status(201).send({ car })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: error.message })
+  }
+})
+
+
+
+
+export default carRouter
